b0659e8a96cde7b744ff60de4ad4dfd72a7d65f4
agus-sideral <rabunovels@gmail.com>
2021-02-16 00:55:26 -0300

    Home: added Container and Appbar

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import { AppBar, Toolbar, Container, Typography } from '@material-ui/core';
 
 export function Home() {
-  return <h1>Welcome to Pokedex</h1>;
+  return (
+    <Container maxWidth="xl">
+      <Container maxWidth="xs">
+      <AppBar color="secondary">
+      <Toolbar>
+        <Typography variant="h6">
+          Welcome to Pokedex
+        </Typography>
+      </Toolbar>
+      </AppBar>
+      </Container>
+    </Container>
+  )
 }
